b876ea646f2fb9d9927ecba1bf79da3a3a76268d
Benson Thomas <benson.thomas2602@gmail.com>
2025-03-05 22:57:00 -0600

    rotating the square

--- example1/init-buffers.js
+++ example1/init-buffers.js
@@ -12,16 +12,16 @@ function initBuffers(gl) {
 function initColorBuffer(gl) {
   const colors = [
     1.0,
-    1.0,
-    1.0,
+    0.0,
+    0.0,
     1.0, // white
-    1.0,
-    0.0,
-    0.0,
+    0.4,
+    0.6,
+    0.3,
     1.0, // red
     0.0,
-    1.0,
     0.0,
+    1.0,
     1.0, // green
     0.0,
     0.0,
