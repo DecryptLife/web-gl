d8b6bade1a68957964727bdb79f19cf115e6f681
Benson Thomas <benson.thomas2602@gmail.com>
2025-03-08 17:44:57 -0600

    element array buffer

--- example3/init-buffers.js
+++ example3/init-buffers.js
@@ -3,31 +3,32 @@ function initBuffers(gl) {
 
   const colorBuffer = initColorBuffer(gl);
 
+  const indexBuffer = initIndexBuffer(gl);
+
   return {
     position: positionBuffer,
     color: colorBuffer,
+    index: indexBuffer,
   };
 }
 
 function initColorBuffer(gl) {
-  const colors = [
-    1.0,
-    0.0,
-    0.0,
-    1.0, // white
-    0.4,
-    0.6,
-    0.3,
-    1.0, // red
-    0.0,
-    0.0,
-    1.0,
-    1.0, // green
-    0.0,
-    0.0,
-    1.0,
-    1.0, // blue
+  const faceColors = [
+    [1.0, 1.0, 1.0, 1.0], // front - white
+    [1.0, 0.0, 0.0, 1.0], // back - red
+    [0.0, 1.0, 0.0, 1.0], // top  - green
+    [0.0, 0.0, 1.0, 1.0], // bottom - blue
+    [1.0, 1.0, 0.0, 1.0], // right - yellow
+    [1.0, 0.0, 1.0, 1.0], // left - purple
   ];
+
+  let colors = [];
+
+  for (const element of faceColors) {
+    const c = element;
+    // repeated four times for four vertices
+    colors = colors.concat(c, c, c, c);
+  }
 
   const colorBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
@@ -71,6 +72,58 @@ function initPositionBuffer(gl) {
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
   return positionBuffer;
+}
+
+function initIndexBuffer(gl) {
+  const indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+  const indices = [
+    0,
+    1,
+    2,
+    0,
+    2,
+    3, // front
+    4,
+    5,
+    6,
+    4,
+    6,
+    7, // back
+    8,
+    9,
+    10,
+    8,
+    10,
+    11, // top
+    12,
+    13,
+    14,
+    12,
+    14,
+    15, // bottom
+    16,
+    17,
+    18,
+    16,
+    18,
+    19, // right
+    20,
+    21,
+    22,
+    20,
+    22,
+    23, // left
+  ];
+
+  gl.bufferData(
+    gl.ELEMENT_ARRAY_BUFFER,
+    new Uint16Array(indices),
+    gl.STATIC_DRAW
+  );
+
+  return indexBuffer;
 }
 
 export { initBuffers };
