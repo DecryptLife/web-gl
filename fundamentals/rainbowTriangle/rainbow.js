26dd77214d7065085126d42dfa2daf06f68495c5
Benson Thomas <benson.thomas2602@gmail.com>
2025-03-15 19:26:26 -0500

    setting up fs vs GLSL

--- fundamentals/rainbowTriangle/rainbow.js
+++ fundamentals/rainbowTriangle/rainbow.js
@@ -2,13 +2,16 @@ const canvas = document.querySelector("#glcanvas");
 const gl = canvas.getContext("webgl");
 
 const vsGLSL = `
+    attribute vec4 aPosition;
     void main() {
+        gl_Position = aPosition;
     }
 `;
 
 const fsGLSL = `
+    precision highp float;
     void main() {
-    
+        gl_FragColor = vec4(1, 0, 0, 1);
     }
 `;
 
