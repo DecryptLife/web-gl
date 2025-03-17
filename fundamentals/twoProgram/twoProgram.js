ec640adb2eda024a935c877312756b0d23252578
Benson Thomas <benson.thomas2602@gmail.com>
2025-03-16 22:44:40 -0500

    initial idea

--- fundamentals/twoProgram/twoProgram.js
+++ fundamentals/twoProgram/twoProgram.js
@@ -2,7 +2,10 @@ const canvas = document.querySelector("#glcanvas");
 
 const gl = canvas.getContext("webgl");
 
-const vsGLSL = `
+gl.clearColor(0.8, 0.4, 0.5, 0.6);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+const vsGLSL1 = `
     attribute vec4 aPosition;
     void main() {
         gl_Position = aPosition;
@@ -10,7 +13,7 @@ const vsGLSL = `
     }
 `;
 
-const fsGLSL = `
+const fsGLSL1 = `
     precision highp float;
 
     void main() {
@@ -19,45 +22,127 @@ const fsGLSL = `
     }
 `;
 
-const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertexShader, vsGLSL);
-gl.compileShader(vertexShader);
-if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-  throw new Error(gl.getShaderInfoLog(vertexShader));
+const vsGLSL2 = `
+  attribute vec4 aPosition;
+  // attribute vec4 aColor;
+
+  varying vec4 v_color;
+  void main() {
+    gl_Position = aPosition;
+    gl_PointSize = 10.0;
+    // v_color = aColor;
+  }
+`;
+
+const fsGLSL2 = `
+  precision highp float;
+
+  varying vec4 v_color;
+  void main() {
+    gl_FragColor = vec4(1.0, 0.2, 0.0, 1.0);
+  }
+`;
+
+const vertexShader1 = gl.createShader(gl.VERTEX_SHADER);
+const vertexShader2 = gl.createShader(gl.VERTEX_SHADER);
+
+gl.shaderSource(vertexShader1, vsGLSL1);
+gl.shaderSource(vertexShader2, vsGLSL2);
+
+gl.compileShader(vertexShader1);
+gl.compileShader(vertexShader2);
+
+if (!gl.getShaderParameter(vertexShader1, gl.COMPILE_STATUS)) {
+  throw new Error(gl.getShaderInfoLog(vertexShader1));
 }
 
-const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragmentShader, fsGLSL);
-gl.compileShader(fragmentShader);
-if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-  throw new Error(gl.getShaderInfoLog(fragmentShader));
+if (!gl.getShaderParameter(vertexShader2, gl.COMPILE_STATUS)) {
+  throw new Error(gl.getShaderInfoLog(vertexShader2));
 }
 
-const prg = gl.createProgram();
-gl.attachShader(prg, vertexShader);
-gl.attachShader(prg, fragmentShader);
-gl.linkProgram(prg);
+const fragmentShader1 = gl.createShader(gl.FRAGMENT_SHADER);
+const fragmentShader2 = gl.createShader(gl.FRAGMENT_SHADER);
 
-const vertexPositions = new Float32Array([
-  0.0, 0.8, 0.0, -0.4,
+gl.shaderSource(fragmentShader1, fsGLSL1);
+gl.shaderSource(fragmentShader2, fsGLSL2);
 
-  -0.2, 0.6, 0.2, 0.6, -0.2, -0.2, 0.2, -0.2,
+gl.compileShader(fragmentShader1);
+gl.compileShader(fragmentShader2);
+if (!gl.getShaderParameter(fragmentShader1, gl.COMPILE_STATUS)) {
+  throw new Error(gl.getShaderInfoLog(fragmentShader1));
+}
+if (!gl.getShaderParameter(fragmentShader2, gl.COMPILE_STATUS)) {
+  throw new Error(gl.getShaderInfoLog(fragmentShader2));
+}
 
-  -0.4, 0.4, 0.4, 0.4, -0.4, 0.0, 0.4, 0.0,
+const prg1 = gl.createProgram();
+gl.attachShader(prg1, vertexShader1);
+gl.attachShader(prg1, fragmentShader1);
+gl.linkProgram(prg1);
 
-  -0.6, 0.2, 0.6, 0.2,
+const prg2 = gl.createProgram();
+gl.attachShader(prg2, vertexShader2);
+gl.attachShader(prg2, fragmentShader2);
+gl.linkProgram(prg2);
+
+const vertexPositions1 = new Float32Array([
+  -0.4, 0.3,
+
+  -0.5, 0.2, -0.3, 0.2,
+
+  -0.6, 0.1, -0.2, 0.1,
+
+  -0.7, 0.0, -0.1, 0.0,
+
+  -0.6, -0.1, -0.2, -0.1,
+
+  -0.5, -0.2, -0.3, -0.2,
+
+  -0.4, -0.3,
 ]);
 
-const positionLoc = gl.getAttribLocation(prg, "aPosition");
+const positionLoc = gl.getAttribLocation(prg1, "aPosition");
 
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STATIC_DRAW);
+gl.bufferData(gl.ARRAY_BUFFER, vertexPositions1, gl.STATIC_DRAW);
 
 gl.enableVertexAttribArray(positionLoc);
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
 
-gl.useProgram(prg);
+const vertexPositions2 = new Float32Array([
+  0.5, 0.4,
+
+  0.5, 0.2,
+
+  0.5, 0,
+
+  0.7, 0,
+
+  0.3, 0,
+
+  0.1, 0,
+
+  0.9, 0,
+
+  0.5, -0.2,
+
+  0.5, -0.4,
+]);
+
+const positionLoc2 = gl.getAttribLocation(prg2, "aPosition");
+
+const positionBuffer2 = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer2);
+gl.bufferData(gl.ARRAY_BUFFER, vertexPositions2, gl.STATIC_DRAW);
+
+gl.enableVertexAttribArray(positionLoc2);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.vertexAttribPointer(positionLoc2, 2, gl.FLOAT, false, 0, 0);
+
+gl.useProgram(prg1);
+gl.useProgram(prg2);
 
 gl.drawArrays(gl.POINTS, 0, 12);
+gl.drawArrays(gl.POINTS, 0, 9);
