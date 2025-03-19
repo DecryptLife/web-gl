c9b07cdb9f00c26f06f2df29730636c41c008656
Benson Thomas <benson.thomas2602@gmail.com>
2025-03-18 23:57:15 -0500

    adding gradient color

--- fundamentals/practice2/practice2.js
+++ fundamentals/practice2/practice2.js
@@ -4,9 +4,13 @@ const gl = canvas.getContext("webgl");
 
 const vsGLSL = `
     attribute vec4 aPosition;
+    attribute vec4 aColor;
+
+    varying vec4 vColor;
 
     void main() {
         gl_Position = aPosition;
+        vColor = aColor;
     }
 
 `;
@@ -14,8 +18,9 @@ const vsGLSL = `
 const fsGLSL = `
     precision highp float;
 
+    varying vec4 vColor;
     void main() {
-        gl_FragColor = vec4(0.8, 0.2, 0.5, 1.0);
+        gl_FragColor = vColor;
     }
 
 `;
@@ -50,7 +55,49 @@ const vertexPositions = new Float32Array([
   0.2, 0.1, 0, -0.3, 0.4, -0.3,
 ]);
 
+const vertexColors = new Uint8Array([
+  255,
+  0,
+  0,
+  255,
+  0,
+  255,
+  0,
+  255,
+  0,
+  0,
+  255,
+  255,
+
+  255,
+  255,
+  0,
+  255, // Yellow (Vertex 4)
+  0,
+  255,
+  255,
+  255, // Cyan (Vertex 5)
+  255,
+  0,
+  255,
+  255, // Magenta (Vertex 6)
+
+  100,
+  100,
+  100,
+  255, // Gray (Vertex 7)
+  200,
+  100,
+  0,
+  255, // Orange (Vertex 8)
+  0,
+  100,
+  200,
+  255, // Light Blue (Vertex 9)
+]);
+
 const positionLoc = gl.getAttribLocation(prg, "aPosition");
+const colorLoc = gl.getAttribLocation(prg, "aColor");
 
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -58,6 +105,14 @@ gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STATIC_DRAW);
 
 gl.enableVertexAttribArray(positionLoc);
 gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.enableVertexAttribArray(colorLoc);
+gl.vertexAttribPointer(colorLoc, 4, gl.UNSIGNED_BYTE, true, 0, 0);
 
 gl.useProgram(prg);
 
