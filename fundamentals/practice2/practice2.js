2333f27a1e7128fdcd5a13d0bb7fdcd5754ba191
Benson Thomas <benson.thomas2602@gmail.com>
2025-03-18 23:27:44 -0500

    project setup

---
+++ fundamentals/practice2/practice2.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector("#glcanvas");
+
+const gl = canvas.getContext("webgl");
+
+const vsGLSL = `
+
+`;
+
+const fsGLSL = `
+
+`;
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
