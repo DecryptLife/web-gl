0babd707367631b16fe88096add2fe96fd1fc092
Benson Thomas <benson.thomas2602@gmail.com>
2025-03-20 00:18:51 -0500

    recollection

--- fundamentals/cube/cube.js
+++ fundamentals/cube/cube.js
@@ -38,8 +38,8 @@ const compileShaderAndLinkProgram = (gl, prg, vsGLSL, fsGLSL) => {
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vsGLSL);
   const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fsGLSL);
 
-  gl.attachShader(vertexShader);
-  gl.attachShader(fragmentShader);
+  gl.attachShader(prg, vertexShader);
+  gl.attachShader(prg, fragmentShader);
 
   gl.linkProgram(prg);
 
@@ -48,3 +48,42 @@ const compileShaderAndLinkProgram = (gl, prg, vsGLSL, fsGLSL) => {
   }
   return prg;
 };
+
+const prg = gl.createProgram();
+compileShaderAndLinkProgram(gl, prg, vsGLSL, fsGLSL);
+
+console.log("Test 1.2 ptg - ", prg);
+
+const vertexPositions = new Float32Array([0.0, 0.2, -0.2, -0.2, 0.2, -0.2]);
+const vertexColors = new Float32Array([1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1]);
+
+const positionLoc = gl.getAttribLocation(prg, "aPosition");
+const colorLoc = gl.getAttribLocation(prg, "aColor");
+const offsetLoc = gl.getUniformLocation(prg, "uOffset");
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STATIC_DRAW);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+console.log("Test 1.2 position loc - ", positionLoc);
+gl.enableVertexAttribArray(positionLoc);
+gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.enableVertexAttribArray(colorLoc);
+gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+
+gl.useProgram(prg);
+
+gl.uniform4fv(offsetLoc, [0.5, 0, 0, 0]);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+gl.uniform4fv(offsetLoc, [-0.5, 0, 0, 0]);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
