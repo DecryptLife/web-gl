0465f403a8667f07daf65d08ceac5e7d4481855c
Benson Thomas <benson.thomas2602@gmail.com>
2025-03-24 22:36:08 -0500

    load shader and init program

--- translation/boilerF/webgl-demo.js
+++ translation/boilerF/webgl-demo.js
@@ -21,7 +21,44 @@ function main() {
   const fsGLSL = `
     precision highp float;
     void main() {
-        gl_FragColor = vec4(1.0 ,0.4, 0.2, 1.0)
+        gl_FragColor = vec4(1.0 ,0.4, 0.2, 1.0);
     }
   `;
+
+  const shaderProgram = initShaderProgram(gl, vsGLSL, fsGLSL);
+}
+
+function initShaderProgram(gl, vsGLSL, fsGLSL) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsGLSL);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsGLSL);
+
+  const shaderProgram = gl.createProgram();
+
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  const linkStatus = gl.getProgramParameter(shaderProgram, gl.LINK_STATUS);
+
+  if (!linkStatus) {
+    throw new Error(gl.getProgramInfoLog(shaderProgram));
+  }
+
+  return shaderProgram;
+}
+
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+
+  gl.shaderSource(shader, source);
+
+  gl.compileShader(shader);
+
+  const compileStatus = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+  if (!compileStatus) {
+    throw new Error(gl.getShaderInfoLog(shader));
+  }
+
+  return shader;
 }
