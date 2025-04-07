f7ff634f8d4124b069ddb3a797d488e63e43f3ee
Benson Thomas <benson.thomas2602@gmail.com>
2025-04-07 00:01:11 -0500

    setting up matrices for transformation

--- translation/boilerF/webgl-demo.js
+++ translation/boilerF/webgl-demo.js
@@ -162,6 +162,23 @@ const main = () => {
     drawScene(gl, programInfo, buffers, translation, rotation, scale);
   };
 
+  const m3 = {
+    translation: (tx, ty) => {
+      return [1, 0, 0, 0, 1, 0, tx, ty, 1];
+    },
+
+    rotation: (angleInRadians) => {
+      const c = Math.cos(angleInRadians);
+      const s = Math.sin(angleInRadians);
+
+      return [c, -s, 0, s, c, 0, 0, 0, 1];
+    },
+
+    scale: (sx, sy) => {
+      return [sx, 0, 0, 0, sy, 0, 0, 0, 1];
+    },
+  };
+
   console.log("Test 1.2 translation - ", translation);
 
   const buffers = initBuffers(gl);
