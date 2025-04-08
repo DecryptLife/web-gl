941430932aadc1bbe1ec85f0d2c8847a2b1ced49
Benson Thomas <benson.thomas2602@gmail.com>
2025-04-07 23:44:05 -0500

    shader code change

--- translation/matrices/webgl-demo.js
+++ translation/matrices/webgl-demo.js
@@ -44,12 +44,11 @@ const main = () => {
         attribute vec2 aPosition;
 
         uniform vec2 uResolution;
-        uniform vec2 uTranslation;
-        uniform vec2 uRotation;
-        uniform vec2 uScale;
+        uniform vec3 uMatrix;
 
         void main(){
-            vec2 zeroToOne = aPosition / uResolution;
+            vec2 position = (uMatrix * vec3(aPosition, 1)).xy;
+            vec2 zeroToOne = position / uResolution;
             vec2 zeroToTwo = zeroToOne * 2.0;
             vec2 clipSpace = zeroToTwo - 1.0;
             gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
