2bef12c590cb86760fde64668f5e7373deb5ca73
Benson Thomas <benson.thomas2602@gmail.com>
2025-04-08 00:45:10 -0500

    bugs fixes - rotation pending

--- translation/matrices/drawScene.js
+++ translation/matrices/drawScene.js
@@ -19,23 +19,29 @@ const drawScene = (
   buffers,
   m3,
   translation,
-  rotation,
+  angleInRadians,
   scale
 ) => {
+  console.log("Translation - ", translation);
   setPositionAttribute(gl, programInfo, buffers);
 
   gl.useProgram(programInfo.program);
 
+  const uResolutionLocation = programInfo.uniformLocations.uniformResolution;
+  const matrixLocation = programInfo.uniformLocations.uniformMatrix;
   const translationMatrix = m3.translation(translation[0], translation[1]);
+  console.log("Translation mat - ", translationMatrix);
   const rotationMatrix = m3.rotation(angleInRadians);
+  console.log("Rotation mat - ", rotationMatrix);
   const scaleMatrix = m3.scale(scale[0], scale[1]);
 
-  const uResolution = programInfo.uniformLocations.uniformResolution;
-  const uTranslation = programInfo.uniformLocations.uniformTranslation;
-  const uRotation = programInfo.uniformLocations.uniformRotation;
-  const uScale = programInfo.uniformLocations.uniformScale;
+  let matrix = m3.multiply(translationMatrix, rotationMatrix);
+  matrix = m3.multiply(matrix, scaleMatrix);
 
-  gl.uniform2f(uResolution, gl.canvas.width, gl.canvas.height);
+  gl.uniform2f(uResolutionLocation, gl.canvas.width, gl.canvas.height);
+
+  console.log("Test matrix - ", matrix);
+  gl.uniformMatrix3fv(matrixLocation, false, matrix);
 
   gl.drawArrays(gl.TRIANGLES, 0, 18);
 };
--- translation/matrices/webgl-demo.js
+++ translation/matrices/webgl-demo.js
@@ -85,7 +85,7 @@ const main = () => {
         attribute vec2 aPosition;
 
         uniform vec2 uResolution;
-        uniform vec3 uMatrix;
+        uniform mat3 uMatrix;
 
         void main(){
             vec2 position = (uMatrix * vec3(aPosition, 1)).xy;
@@ -103,6 +103,63 @@ const main = () => {
         }
   `;
 
+  const m3 = {
+    translation: (tx, ty) => {
+      return [1, 0, 0, 0, 1, 0, tx, ty, 1];
+    },
+
+    rotation: (angleInRadians) => {
+      const c = Math.cos(angleInRadians);
+      const s = Math.sin(angleInRadians);
+
+      return [c, -s, 0, s, c, 0, 0, 0, 1];
+    },
+
+    scale: (sx, sy) => {
+      return [sx, 0, 0, 0, sy, 0, 0, 0, 1];
+    },
+
+    multiply: (a, b) => {
+      const a00 = a[0 * 3 + 0];
+      const a01 = a[0 * 3 + 1];
+      const a02 = a[0 * 3 + 2];
+
+      const a10 = a[1 * 3 + 0];
+      const a11 = a[1 * 3 + 1];
+      const a12 = a[1 * 3 + 2];
+
+      const a20 = a[2 * 3 + 0];
+      const a21 = a[2 * 3 + 1];
+      const a22 = a[2 * 3 + 2];
+
+      const b00 = b[0 * 3 + 0];
+      const b01 = b[0 * 3 + 1];
+      const b02 = b[0 * 3 + 2];
+
+      const b10 = b[1 * 3 + 0];
+      const b11 = b[1 * 3 + 1];
+      const b12 = b[1 * 3 + 2];
+
+      const b20 = b[2 * 3 + 0];
+      const b21 = b[2 * 3 + 1];
+      const b22 = b[2 * 3 + 2];
+
+      return [
+        b00 * a00 + b01 * a10 + b02 * a20,
+        b00 * a01 + b01 * a11 + b02 * a21,
+        b00 * a02 + b01 * a12 + b02 * a22,
+
+        b10 * a00 + b11 * a10 + b12 * a20,
+        b10 * a01 + b11 * a11 + b12 * a21,
+        b10 * a02 + b11 * a12 + b12 * a22,
+
+        b20 * a00 + b21 * a10 + b22 * a20,
+        b20 * a01 + b21 * a11 + b22 * a21,
+        b20 * a02 + b21 * a12 + b22 * a22,
+      ];
+    },
+  };
+
   const shaderProgram = loadShaderProgram(gl, vsGLSL, fsGLSL);
 
   const programInfo = {
@@ -112,11 +169,13 @@ const main = () => {
     },
     uniformLocations: {
       uniformResolution: gl.getUniformLocation(shaderProgram, "uResolution"),
-      uniformTranslation: gl.getUniformLocation(shaderProgram, "uTranslation"),
-      uniformRotation: gl.getUniformLocation(shaderProgram, "uRotation"),
-      uniformScale: gl.getUniformLocation(shaderProgram, "uScale"),
+      uniformMatrix: gl.getUniformLocation(shaderProgram, "uMatrix"),
     },
   };
+
+  let translation = [0, 0];
+  let angleInRadians = 0;
+  let scale = [1, 1];
 
   const updatePosition = (index) => {
     translation[index] =
@@ -142,7 +201,7 @@ const main = () => {
   };
 
   const buffers = initBuffers(gl);
-  drawScene(gl, programInfo, buffers);
+  drawScene(gl, programInfo, buffers, m3, translation, rotation, scale);
 };
 
 main();
